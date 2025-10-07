cf396cf8a92636a2b4235ed5a1034098cba0dc14
dan.chirila <dan.chirila@m-ware.eu>
2025-10-07 14:10:58 +0300

    [axolotl-ray] Rollback and keep a simple densemax image for dev

---
+++ axolotl-ray/train.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+CONFIG=$WORK_DIR/axolotl_solved.yaml
+
+echo "Downloading model ${BASE_MODEL}"
+lakectl fs download -r lakefs://$BASE_MODEL/ $WORK_DIR/model
+
+echo "Preparing config file"
+envsubst < $WORK_DIR/axolotl.yaml > $CONFIG
+
+if [ -n "${DEEPSPEED}" ]; then
+  echo "DeepSpeed is enabled. Fetching configs"
+  axolotl fetch deepspeed_configs
+fi
+
+echo "Downloading dataset ${DATASET}"
+lakectl fs download -r lakefs://$DATASET/ $WORK_DIR/dataset
+
+echo "Preprocessing dataset"
+axolotl preprocess $CONFIG
+
+echo "Training base model: ${BASE_MODEL}"
+axolotl train $CONFIG
+
+echo "Merging LoRA into the base model"
+axolotl merge-lora $CONFIG --lora-model-dir=$WORK_DIR/outputs/$BASE_MODEL
+
+if [[ "${TORCHAO}" == "true" ]]; then
+  echo "Running quantization using Axolotl torchAO (Inference not working on Ampere GPU)"
+  axolotl quantize $CONFIG --base-model=$WORK_DIR/outputs/$BASE_MODEL/merged/
+else
+  echo "Running quantization using LLMCompressor"
+  python /scripts/quantization.py
+fi
+
+echo "Uploading final model to a new branch"
+lakectl branch create lakefs://$BASE_MODEL-$BRANCH -s lakefs://$BASE_MODEL
+lakectl fs upload -rs $WORK_DIR/outputs/$BASE_MODEL/quantized/ lakefs://$BASE_MODEL-$BRANCH/
