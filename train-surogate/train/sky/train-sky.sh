539ae8095539ff744d660c391c5bad0c7a454521
dan.chirila <dan.chirila@m-ware.eu>
2026-02-18 11:48:01 +0200

    [train-surogate] fixes

--- train-surogate/train/sky/train-sky.sh
+++ train-surogate/train/sky/train-sky.sh
@@ -2,6 +2,7 @@
 
 set -e
 mkdir -p /opt/aim
+mkdir -p /opt/work/outputs
 
 export PYTHONUNBUFFERED=1
 CONFIG=$WORK_DIR/config_solved.yaml
@@ -10,4 +11,4 @@ echo "Preparing config file"
 echo "$AXOLOTL_CONFIG" > $CONFIG
 
 echo "Training base model: ${BASE_MODEL}"
-.venv/bin/surogate sft $CONFIG
+surogate sft $CONFIG
